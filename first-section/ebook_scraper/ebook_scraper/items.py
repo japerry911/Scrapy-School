f22f903ec0b685f59392656ce1f88850c929806f
Jack Perry <jack.e.perry@outlook.com>
2022-09-09 06:46:42 -0600

    👌 IMPROVE: progress on item loader section

--- first-section/ebook_scraper/ebook_scraper/items.py
+++ first-section/ebook_scraper/ebook_scraper/items.py
@@ -1,6 +1,23 @@
 from scrapy import Field, Item
+from itemloaders.processors import MapCompose, TakeFirst
+
+
+def get_price(txt: str) -> float:
+    return float(txt.replace("£", ""))
+
+
+def convert_to_usd_dollars(pounds: float) -> float:
+    return pounds * 0.89
 
 
 class EbookItem(Item):
-    title = Field()
-    price = Field()
+    title = Field(
+        output_processor=TakeFirst()
+    )
+    price = Field(
+        input_processor=MapCompose(
+            get_price,
+            convert_to_usd_dollars
+        ),
+        output_processor=TakeFirst()
+    )
--- first-section/ebook_scraper/ebook_scraper/spiders/ebook.py
+++ first-section/ebook_scraper/ebook_scraper/spiders/ebook.py
@@ -1,4 +1,5 @@
 import scrapy
+from scrapy.loader import ItemLoader
 
 from ebook_scraper.items import EbookItem
 
@@ -6,18 +7,31 @@ from ebook_scraper.items import EbookItem
 class EbookSpider(scrapy.Spider):
     name = "ebook"
 
-    start_urls = ["https://books.toscrape.com/"]
+    # start_urls = ["https://books.toscrape.com/"]
+    start_urls = [
+        "https://books.toscrape.com/catalogue/category/books/travel_2/"
+    ]
 
     def parse(self, response: scrapy.http.Response, **_kwargs):
         ebooks = response.css("article.product_pod")
 
         for ebook in ebooks:
-            ebook_item = EbookItem()
+            loader = ItemLoader(
+                item=EbookItem(),
+                selector=ebook
+            )
+            # ebook_item = EbookItem()
 
-            ebook_item["title"] = ebook.css("h3 a").attrib["title"]
+            loader.add_css("title", "h3 a::attr(title)")
+            # ebook_item["title"] = ebook.css("h3 a").attrib["title"]
             # shorter example: title = ebook.css("h3 a::attr(title)").get()
-            ebook_item["price"] = ebook.css(
+            loader.add_css(
+                "price",
                 "div.product_price p.price_color::text"
-            ).get()
+            )
+            # ebook_item["price"] = ebook.css(
+            #     "div.product_price p.price_color::text"
+            # ).get()
 
-            yield ebook_item
+            # yield ebook_item
+            yield loader.load_item()
