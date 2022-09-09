ae8ecb59366854b74d9b0a720806ce1a919d1c64
Jack Perry <jack.e.perry@outlook.com>
2022-09-09 06:35:42 -0600

    👌 IMPROVE: added items and ipython dev

--- first-section/ebook_scraper/ebook_scraper/items.py
+++ first-section/ebook_scraper/ebook_scraper/items.py
@@ -1,12 +1,6 @@
-# Define here the models for your scraped items
-#
-# See documentation in:
-# https://docs.scrapy.org/en/latest/topics/items.html
-
-import scrapy
+from scrapy import Field, Item
 
 
-class EbookScraperItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+class EbookItem(Item):
+    title = Field()
+    price = Field()
--- first-section/ebook_scraper/ebook_scraper/spiders/ebook.py
+++ first-section/ebook_scraper/ebook_scraper/spiders/ebook.py
@@ -1,5 +1,7 @@
 import scrapy
 
+from ebook_scraper.items import EbookItem
+
 
 class EbookSpider(scrapy.Spider):
     name = "ebook"
@@ -7,7 +9,15 @@ class EbookSpider(scrapy.Spider):
     start_urls = ["https://books.toscrape.com/"]
 
     def parse(self, response: scrapy.http.Response, **_kwargs):
-        print("[ parse ]")
+        ebooks = response.css("article.product_pod")
 
-        response.css("h3 a::text")
-        response.xpath("//h3/a/text()")
+        for ebook in ebooks:
+            ebook_item = EbookItem()
+
+            ebook_item["title"] = ebook.css("h3 a").attrib["title"]
+            # shorter example: title = ebook.css("h3 a::attr(title)").get()
+            ebook_item["price"] = ebook.css(
+                "div.product_price p.price_color::text"
+            ).get()
+
+            yield ebook_item
